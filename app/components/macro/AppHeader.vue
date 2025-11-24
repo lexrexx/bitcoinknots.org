8519507356475bfb9bce0e9057b25908575de892
Lex Rex <lexnaturale@pm.me>
2025-11-24 12:56:37 +0200

    Revert from a failed color-test

--- app/components/macro/AppHeader.vue
+++ app/components/macro/AppHeader.vue
@@ -4,7 +4,7 @@
   >
     <NuxtLink
       to="/"
-      class="flex justify-center items-center bg-[#FD7E14] dark:bg-brand border-onyx/10 dark:border-vanil/10 border-r w-20 md:w-24 h-20 md:h-24"
+      class="flex justify-center items-center bg-vanil dark:bg-brand border-onyx/10 dark:border-vanil/10 border-r w-20 md:w-24 h-20 md:h-24"
       @click="menu = false"
     >
       <AppLogomark class="w-12 md:w-14 h-12 md:h-14 text-onyx" />
