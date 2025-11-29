df93a048d67d76bd670688bdd2db02af2c55184c
Lex Rex <lexnaturale@pm.me>
2025-11-29 12:43:22 +0200

    Remove internal page

--- app/app.vue
+++ app/app.vue
@@ -65,4 +65,23 @@ html {
   white-space: nowrap;
   border: 0;
 }
+
+body {
+  @apply antialiased
+  text-onyx dark:text-vanil
+  bg-brand dark:bg-onyx
+  selection:text-vanil dark:selection:text-onyx
+  selection:bg-onyx dark:selection:bg-vanil;
+}
+
+a {
+  @apply text-vanil dark:text-brand;
+}
+
+p {
+  @apply
+    py-2 md:py-4
+    text-base sm:text-base md:text-lg lg:text-xl
+    leading-relaxed lg:leading-normal
+}
 </style>
